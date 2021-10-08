82f1a305b17ecb65e5f6352deed1ec31deb78742
Joshua Dsylva <joshdsy@gmail.com>
2021-10-08 16:11:28 +0530

    added scripts for k8s-hetzner

---
+++ terraform-provisioning/k8s-hetzner/scripts/master.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/d
+ebian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update && sudo apt-get -y install containerd.io
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+sudo kubeadm init --apiserver-advertise-address 192.168.0.2 \
+    --service-cidr 10.96.0.0/16 \
+    --pod-network-cidr 10.244.0.0/16 \
+    --ignore-preflight-errors=NumCPU
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
